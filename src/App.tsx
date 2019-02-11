4438e5f353e68cacbdde0798ab35af24211ec23c
Rafael Freitas da Silva <rfl.fsilva@gmail.com>
2019-02-11 10:59:35 -0200

    Adds sample call using Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,64 @@
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+interface CharacterListState {
+  characters: any;
+}
+
+
+class App extends Component<{}, CharacterListState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { characters: [] };
+  }
+
+  componentDidMount() {
+    const client = new ApolloClient({
+      uri: "https://rickandmortyapi.com/graphql"
+    });
+
+    client.query<any>({
+      query: gql`
+        {
+          characters(,filter: { name: "rick" }) {
+            results {
+              id
+              name
+            }
+          }
+        }
+      `
+    })
+    .then(result => {
+      this.setState((state, props) => ({
+        ...state,
+        characters: result.data.characters.results
+      }));
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CharacterList characters={this.state.characters} />
       </div>
     );
   }
+}
+
+
+function CharacterList(props: any) {
+  const characters = props.characters;
+  const listCharacters = characters.map((character: any) =>
+   <p key={character.id}>{character.name}</p>
+  );
+  return (
+    <div>{listCharacters}</div>
+  );
 }
 
 export default App;
